eec7e189d4bba5d4d358a2df745ff476eef4706d
Szyken <143886338+Szyken@users.noreply.github.com>
2025-12-24 16:03:08 +0700

    feat: add home page hero section component and introduce `sharp` dependency.

--- src/components/(pages)/home/hero-section.tsx
+++ src/components/(pages)/home/hero-section.tsx
@@ -13,7 +13,7 @@ export default function Hero() {
           <div className="text-wrapper">
             <h1>
               <span>HIMSI</span>
-              <span>&nbsp;GEN</span> XV
+              <span>&nbsp;GEN</span> XVI
             </h1>
             <p>
               In the long book of life, every family has its own story. <br />
