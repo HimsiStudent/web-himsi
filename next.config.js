d204002d9401501df4fba523e55448841db44edd
Szyken <himsistudent@gmail.com>
2026-01-06 16:40:47 +0700

    Fix gambar error

--- next.config.js
+++ next.config.js
@@ -1,10 +1,8 @@
 /** @type {import('next').NextConfig} */
-
-module.exports = nextConfig;
-
 const nextConfig = {
     images: {
         unoptimized: true,
     },
 };
 
+module.exports = nextConfig;
